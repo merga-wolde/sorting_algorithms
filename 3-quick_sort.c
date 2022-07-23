a8d4cebad1bb8aa35b6649f59e9cb9d86dc7747e
merga-wolde <mergawolde@gmail.com>
2022-07-23 20:52:37 +0300

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,74 +1,69 @@
 #include "sort.h"
-#include <stdio.h>
 /**
- * quick_sort - sorts an array of integers in ascending order
- * using the quick sort sort algorithm
- * @array: pointer to the array
- * @size: size of the array
-*/
-void quick_sort(int *array, size_t size)
+ * partition - sort the selected section
+ * @array: Array of data to be sorted
+ * @low: Beggining of the selected list
+ * @high: End of the selected list
+ * @size: Size of the array
+ * Return: index increased from low if there is a swap
+ */
+int partition(int *array, int low, int high, size_t size)
 {
-recursive_quick_sort(array, size, 0, size - 1);
-}
+	int pivot = array[high], tmp = 0, i = low - 1, j = low;
 
-/**
- * recursive_quick_sort - recursive part
- * @array: array to use
- * @size: size
- * @start: start index
- * @end: end index
-*/
-void recursive_quick_sort(int *array, size_t size, int start, int end)
-{
-int p;
-if (start < end)
-{
-	p = partition(array, size, start, end);
-
-	recursive_quick_sort(array, size, start, p - 1);
-	recursive_quick_sort(array, size, p + 1, end);
-}
-
-}
-
-/**
- * partition - partition the array
- * @array: array to use
- * @size: size
- * @start: start index
- * @end: end index
- * Return: partition index
-*/
-size_t partition(int *array, size_t size, int start, int end)
-{
-int pivot = array[end];
-int i = start - 1;
-int j;
-
-for (j = start; j <= end - 1; j++)
-{
-	if (array[j] < pivot)
+	while (j <= high - 1)
 	{
-		i++;
-		swap_int1(array, i, j);
+		if (array[j] <= pivot)
+		{
+			i++;
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+			if (i != j)
+				print_array(array, size);
+		}
+		j++;
+	}
+	i++;
+	tmp = array[i];
+	array[i] = array[high];
+	array[high] = tmp;
+	if (array[i] != array[high])
 		print_array(array, size);
+	return (i);
+}
+/**
+ * quick - Quicksort is a divide-and-conquer algorithm. It works by
+ * selecting a 'pivot' element from the array and partitioning the other
+ * elements into two sub-arrays, according to whether they are less than
+ * or greater than the pivot. The sub-arrays are then sorted recursively.
+ * @array: Array of data to be sorted
+ * @low: Beggining of the selected list
+ * @high: End of the selected list
+ * @size: Size of the array
+ */
+void quick(int *array, int low, int high, size_t size)
+{
+	int p = 0;
+
+	if (low < high)
+	{
+		p = partition(array, low, high, size);
+		quick(array, low, p - 1, size);
+		quick(array, p + 1, high, size);
 	}
 }
-swap_int1(array, i + 1, end);
-print_array(array, size);
-return (i+1);
-}
-
 /**
- * swap_int1 - swap variable values
- * @array: array to use
- * @a: index 1
- * @b: index 2
-*/
-void swap_int1(int *array, int a, int b)
+ * quick_sort - Quicksort is a divide-and-conquer algorithm. It works by
+ * selecting a 'pivot' element from the array and partitioning the other
+ * elements into two sub-arrays, according to whether they are less than
+ * or greater than the pivot. The sub-arrays are then sorted recursively.
+ * @array: Array of data to be sorted
+ * @size: Size of the array
+ */
+void quick_sort(int *array, size_t size)
 {
-int tmp;
-tmp = array[a];
-array[a] = array[b];
-array[b] = tmp;
+	if (size < 2)
+		return;
+	quick(array, 0, size - 1, size);
 }
