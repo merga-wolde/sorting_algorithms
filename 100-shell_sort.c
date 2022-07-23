e2054b3f7f9812ac5fb80f05049e4687d80683dc
merga-wolde <mergawolde@gmail.com>
2022-07-23 20:58:26 +0300

    shell

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,37 +1,37 @@
 #include "sort.h"
-
 /**
- * shell_sort - sort algorithm
- * @array: array to sort
- * @size: size of the array
+ * shell_sort - This algorithm uses insertion sort on a widely
+ * spread elements, first to sort them and then sorts the less widely
+ * spaced elements. This spacing is termed as interval. This interval
+ * is calculated based on Knuth's formula as − h = h * 3 + 1
+ * @array: Array of data to be sorted
+ * @size: Size of the array
  */
 void shell_sort(int *array, size_t size)
 {
-int inner, outer;
-int valueToInsert;
-int interval = 1;
-int i = 0;
+	size_t gap = 1, i = 0, j = 0;
+	int tmp = 0;
 
-while(interval <= (int) size/3)
-	interval = interval*3 +1;
-
-
-while(interval > 0) {
-
-	for(outer = interval; outer < (int) size; outer++) {
-		valueToInsert = array[outer];
-		inner = outer;
-
-		while(inner > interval -1 && array[inner - interval] >= valueToInsert) {
-		array[inner] = array[inner - interval];
-		inner -=interval;
+	if (size < 2)
+		return;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+	while (gap > 0)
+	{
+		j = gap;
+		while (j < size)
+		{
+			tmp = array[j];
+			i = j;
+			while (i > gap - 1 && array[i - gap] >= tmp)
+			{
+				array[i] = array[i - gap];
+				i -= gap;
+			}
+			array[i] = tmp;
+			j++;
 		}
-
-		array[inner] = valueToInsert;
+		gap = (gap - 1) / 3;
+		print_array(array, size);
 	}
-
-interval = (interval -1) /3;
-print_array(array, size);
-i++;
-}
 }
